2e28781554c01b9f21945a2692e9489b33602e95
SageSG <houliangyip@gmail.com>
2022-11-17 15:32:15 +0800

    3x03 quiz

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import passwordData from './10-million-password-list-top-1000.txt';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { password: "", passwordList : []};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    // make fetch request
+    this.state.passwordList = fetch(passwordData)
+      .then(r => r.text())
+      .then(text => {
+        return text.split('\r\n');
+      });
+
+}
+
+  handleChange(event) {
+    this.setState({ password: event.target.value }, () => {
+      console.log('updating')
+      });
+  }
+
+  handleClick(e) {
+    e.preventDefault()
+
+    console.log('Click happened');
+    const password = this.state.password;
+
+
+    console.log(this.state.passwordList)
+    
+
+    // if (this.state.passwordList.includes(this.state.password)) {
+    //   alert("Password is too common");
+    //   return;
+    // }
+    if (password.length < 8) {
+      alert("Password must be at least 8 characters");
+      return;
+    }
+    if (password.length > 30) {
+      alert("Password cannot be this long")
+      return;
+    }
+
+
+
+  }
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
@@ -11,7 +62,9 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+
+          <div>Password</div><input type="text" onChange={this.handleChange.bind(this)}></input>
+          <button onClick={this.handleClick.bind(this)}>Submit</button>
         </p>
       </div>
     );
